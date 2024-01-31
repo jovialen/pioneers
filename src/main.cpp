330fb1ce9e85aa3fab9b2e6c62a0d81411dbca60
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2024-01-31 09:58:49 +0100

    feat: configure custom default logger

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <vector>
+
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/spdlog.h>
+
+void configure_logger() {
+	std::vector<spdlog::sink_ptr> sinks;
+	sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+	sinks.push_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/pioneers.log", true));
+	auto logger = std::make_shared<spdlog::logger>("Pioneers", sinks.begin(), sinks.end());
+	spdlog::set_default_logger(logger);
+}
 
 int main(int argc, char *argv[]) {
-  std::cout << "Hello, world!" << std::endl;
-  return 0;
+	configure_logger();
+	spdlog::info("Hello, world!");
+	return 0;
 }
